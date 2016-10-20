b77820709d8779c6eb74ff8122b9188908b1cd7d
Daniel Nieto <danielnieto89@gmail.com>
2016-10-20 17:44:57 -0500

    change project name to "Scrapman"
    update all files to reflect this change name

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# GetReal
-get *real* HTML code from an URL, ultra fast and simple (ideal for web scrapping)
+# Scrapman
+
+*Ski-bi dibby dib yo da dub dub*<br>
+*Yo da dub dub*<br>
+*Ski-bi dibby dib yo da dub dub*<br>
+*Yo da dub dub*<br>
+
+###I'm the Scrapman!
+
+>Retrieve **real (with Javascript executed)** HTML code from an URL, ultra fast and supports unlimited parallel loading of webs
--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const spawn = require("child_process").spawn
 const electronPath = require("electron");
 
 
-var getRealPath = path.join(__dirname, './lib/getreal.js');
+var scrapmanPath = path.join(__dirname, './lib/scrapman.js');
 
-let child = spawn(electronPath, [getRealPath], { stdio: ["pipe", "pipe", "pipe", "ipc"] });
-let getRealIsReady = false;
+let child = spawn(electronPath, [scrapmanPath], { stdio: ["pipe", "pipe", "pipe", "ipc"] });
+let scrapmanIsReady = false;
 let actionsQueue = [];
 
 const ipcManager_parent = require("ipc-messages-manager").parent;
 
 ipcManager_parent.send(child, "notify-when-ready", null, function() {
-    getRealIsReady = true;
+    scrapmanIsReady = true;
     executeQueue();
 });
 
@@ -24,7 +24,7 @@ let queueAction = (action, params, callback) => {
 }
 
 let executeQueue = () => {
-    // execute all pending actions, this will be executed once the "getReal"(Electron) instance is ready
+    // execute all pending actions, this will be executed once the "scrapman"(Electron) instance is ready
     actionsQueue.forEach((action) => {
         // send the actual message to the child
         ipcManager_parent.send(child, action.action, action.params, function(result) {
@@ -37,13 +37,13 @@ let executeQueue = () => {
 }
 
 let load = (url, callback) => {
-    if (getRealIsReady) {
-        // if getReal instance is ready, immediately send the message to child
+    if (scrapmanIsReady) {
+        // if scrapman instance is ready, immediately send the message to child
         ipcManager_parent.send(child, "load", {url}, function(result) {
             callback(result);
         });
     } else {
-        // if the getReal instance is not ready yet, then add the function to the queue
+        // if the scrapman instance is not ready yet, then add the function to the queue
         queueAction("load", {url}, callback);
     }
 }
