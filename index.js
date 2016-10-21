ee2f1a9d8b208108f30809de073b294d0410c4b8
Daniel Nieto <danielnieto89@gmail.com>
2016-10-21 15:08:32 -0500

    limit concurrent operations to 50, and use a Queue to handle pending operations
    to avoid clogging the execution, handle the operations at chunks of 50,
    this number should be later customizable

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ var scrapmanPath = path.join(__dirname, './lib/scrapman.js');
 let child = spawn(electronPath, [scrapmanPath], { stdio: ["pipe", "pipe", "pipe", "ipc"] });
 let scrapmanIsReady = false;
 let onceReadyActionsQueue = [];
+let actionsQueue = [];
+let maxConcurrentOperations = 50;
+let currentConcurrentOperations = 0;
 
 const ipcManager_parent = require("ipc-messages-manager").parent;
 
@@ -26,24 +29,52 @@ let queueOnceReadyAction = (action, params, callback) => {
 let executeOnceReadyQueue = () => {
     // execute all pending actions, this will be executed once the "scrapman"(Electron) instance is ready
     onceReadyActionsQueue.forEach((action) => {
-        // send the actual message to the child
-        ipcManager_parent.send(child, action.action, action.params, function(result) {
-            action.callback(result);
-        });
+        // try to send the message to the child
+        executeAction(action.action, action.params, action.callback);
     });
 
     // clean up the queue
     onceReadyActionsQueue = [];
 }
 
+let executeRemainingQueuedActions = () => {
+    if(actionsQueue.length > 0){
+        let queuedAction = actionsQueue.shift();
+        executeAction(queuedAction.action, queuedAction.params, queuedAction.callback);
+    }
+}
+
+//this function limits concurrent execution of several actions,
+//to prevent the Three Stooges Syndrome (clogging above 50 concurrent operations)
+let executeAction = (action, params, callback) => {
+    // check if more concurrent operations can be executed
+    if(currentConcurrentOperations < maxConcurrentOperations){
+
+        // increment the "councurrent operations" counter
+        currentConcurrentOperations++;
+
+        // send the actual message to the child
+        ipcManager_parent.send(child, action, params, function(results){
+            callback(results);
+            // once this concurrent operation finished, decrement the "concurrent oprations" counter
+            currentConcurrentOperations--;
+            // if there are pending operations on queue to be performed, then process them.
+            executeRemainingQueuedActions();
+        });
+
+    }else{
+        // all concurrent slots are occupied, queue this operation.
+        actionsQueue.push({action, params, callback});
+    }
+}
+
 let load = (url, callback) => {
     if (scrapmanIsReady) {
-        // if scrapman instance is ready, immediately send the message to child
-        ipcManager_parent.send(child, "load", {url}, function(result) {
-            callback(result);
-        });
+        // if scrapman instance is ready, try to send the action to the child
+        executeAction("load", {url}, callback);
     } else {
-        // if the scrapman instance is not ready yet, then add the function to the queue
+        // if the scrapman instance is not ready yet, then add the function
+        // to the queue that will get executed once it's ready
         queueOnceReadyAction("load", {url}, callback);
     }
 }
